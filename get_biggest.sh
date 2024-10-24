c84de318e3fcdc023f7b9775c9acf31b42e86351
Kivanc Gordu <kivancgordu@hotmail.com>
2024-10-24 22:27:07 +0200

    Add get_biggest.sh and .gitignore

---
+++ get_biggest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check whether path exists, give error in case
+if [ -z "$1" ]; then
+  echo "Invalid path or folder name. Please check the input and try again."
+  exit 1
+fi
+
+# Get first parameter
+path=$1
+file_amount=10
+output_file=/tmp/filesize.txt
+
+# Introduction
+echo "This program ($0) retrieves top $file_amount largest files in $1 as $output_file."
+
+
+# Print path
+echo "Given path: $path"
+
+# Get the top 10 biggest files
+du -ah $path | sort -rh | head -n $file_amount > $output_file
+
+# Display the results
+cat $output_file
+
